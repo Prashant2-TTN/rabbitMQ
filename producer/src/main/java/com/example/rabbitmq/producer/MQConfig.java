118a4bbd5e7566630d32363e0505558952f54b4f
Prashant <prashant.gupta2@tothenew.com>
2025-05-01 17:28:21 +0530

    implemented producer

---
+++ producer/src/main/java/com/example/rabbitmq/producer/MQConfig.java
@@ -0,0 +1,48 @@
+package com.example.rabbitmq.producer;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MQConfig {
+
+    public static final String messageQueue = "message_queue";
+    public static final String topicExchange = "topic_exchange";
+    public static final String messageRoutingKey = "message_routing_key";
+
+    @Bean
+    public Queue queue(){
+        return new Queue(messageQueue);
+    }
+
+    @Bean
+    public TopicExchange exchange(){
+        return new TopicExchange(topicExchange);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange){
+        return BindingBuilder.bind(queue).to(exchange).with(messageRoutingKey);
+    }
+
+    @Bean
+    public MessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate template(ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate=new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+
+    }
+
+
+}
